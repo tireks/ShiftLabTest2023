0681dae39d4bafa29c42a67ef5b090f89bd59719
tireks <33616425+tireks@users.noreply.github.com>
2023-09-19 18:43:46 +0700

    base navigation setted

---
+++ app/src/main/java/com/example/shiftlabtest2023/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.shiftlabtest2023
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import com.example.shiftlabtest2023.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val navController get() = findNavController(R.id.mainDataContainer)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
+    }
+}
