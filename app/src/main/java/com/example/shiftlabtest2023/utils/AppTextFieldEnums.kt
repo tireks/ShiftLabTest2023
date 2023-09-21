f8a99312df9bd59099dace7b1bd4a4fa4a33f2c7
tireks <33616425+tireks@users.noreply.github.com>
2023-09-21 13:31:00 +0700

    support utilits

---
+++ app/src/main/java/com/example/shiftlabtest2023/utils/AppTextFieldEnums.kt
@@ -0,0 +1,9 @@
+package com.example.shiftlabtest2023.utils
+
+enum class AppTextFieldEnums(var content : String) {
+    Name(""),
+    Surname(""),
+    Birthdate(""),
+    Password(""),
+    PasswordConf("")
+}
