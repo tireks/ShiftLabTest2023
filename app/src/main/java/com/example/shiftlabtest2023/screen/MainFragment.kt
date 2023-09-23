b67a2744d35981ac7850e83611d59bbdf1e54a52
tireks <33616425+tireks@users.noreply.github.com>
2023-09-23 12:01:21 +0700

    base functional
    WITHOUT logic for now

---
+++ app/src/main/java/com/example/shiftlabtest2023/screen/MainFragment.kt
@@ -0,0 +1,55 @@
+package com.example.shiftlabtest2023.screen
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
+import com.example.shiftlabtest2023.R
+import com.example.shiftlabtest2023.databinding.FragmentMainBinding
+import com.example.shiftlabtest2023.utils.mainActivity
+
+class MainFragment : BaseFragment<FragmentMainBinding>(){
+
+    override fun inflateViewBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentMainBinding {
+        return FragmentMainBinding.inflate(inflater, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mainActivity.setSupportActionBar(binding.mainToolbar)
+        setupMenu()
+    }
+
+    private fun performAccountRemoval() {
+        
+    }
+
+    private fun setupMenu() {
+        (requireActivity() as MenuHost).addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.main_manu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when (menuItem.itemId) {
+                    R.id.delete_menu_button -> {
+                        performAccountRemoval()
+                        true
+                    }
+                    else -> return false
+                }
+            }
+
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
+}
